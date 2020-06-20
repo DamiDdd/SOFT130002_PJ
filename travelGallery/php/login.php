f03d7e6f5c6d66dca4b1266c2237f26b2271d8f7
e0392448 <834767813@qq.com>
2020-06-20 23:27:58 +0800

    pj2

---
+++ travelGallery/php/login.php
@@ -0,0 +1,2 @@
+欢迎<?php echo $_POST("username"); ?><br>
+你的密码<?php echo $_POST("password"); ?>
--- travelGallery/php/test.php
+++
@@ -1,5 +0,0 @@
-<?php
-$db = new MySQLi("localhost","root","password","travelstore");
-!mysqli_connect_error() or die("连接失败");
-
-?>
