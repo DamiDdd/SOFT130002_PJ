749c509898d8e1f95518338c8ae1098c1a475158
e0392448 <834767813@qq.com>
2020-06-21 11:46:16 +0800

    pj2

---
+++ travelGallery/php/config.php
@@ -0,0 +1,7 @@
+<?php
+define('DBHOST', 'localhost');
+define('DBNAME', 'travelgallery');
+define('DBUSER', 'user');
+define('DBPASS', 'password');
+define('DBCONNSTRING','mysql:dbname=travelgallery;charset=utf8mb4;');
+?>
--- travelGallery/php/login.php
+++
@@ -1,2 +0,0 @@
-欢迎<?php echo $_POST("username"); ?><br>
-你的密码<?php echo $_POST("password"); ?>
